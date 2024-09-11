eadc96d9e57e744f3bd7beaf6983148c24c6e8a2
Giovanna Esteves <gio_esteves@hotmail.com>
2024-09-11 14:12:49 -0300

    adequação para a main

--- backend/core.py
+++ backend/core.py
@@ -28,32 +28,10 @@ def atualizar_estoque(nome, quantidade): # UPDATE
             return f"Quantidade do produto '{nome}' atualizada!"
     return "Produto não encontrado."
 
-<<<<<<< HEAD
-=======
-# DELETE
-
-# teste unitário
-# adicionar_produto("laranja", "fruta", 30, 15.66)
-# print(estoque)
-# atualizar_estoque("laranja", 2)
-# print(estoque)
-
-def listar_produtos(): # READ
-    if not estoque:
-        return "Estoque vazio."
-    
-    for produto in estoque:
-        print(f"Nome: {produto['nome']}, Categoria: {produto['categoria']}, Quantidade: {produto['quantidade']}, Preço: {produto['preco']:.2f}")
-
->>>>>>> 3207bc9377864f8daa63b28bb977930aa4d354e5
 def remover_produto(nome): # DELETE
     for produto in estoque:
         if produto['nome'] == nome:
             estoque.remove(produto)
             return f"Produto '{nome}' removido com sucesso."
     
-<<<<<<< HEAD
     return "Produto não encontrado."
-=======
-    return "Produto não encontrado."
->>>>>>> 3207bc9377864f8daa63b28bb977930aa4d354e5
--- frontend/ui.py
+++ frontend/ui.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import messagebox
-<<<<<<< HEAD
 from backend.core import adicionar_produto, listar_produtos, atualizar_estoque, remover_produto, estoque
 
 def cadastrar_produto():
@@ -100,48 +99,3 @@ def criar_interface():
 
     # Inicia o loop principal da interface gráfica
     root.mainloop()
-=======
-
-def cadastrar_produto():
-    messagebox.showinfo('Cadastrar produto')
-  
-def localizar_produto():
-    messagebox.showinfo('Localizar produto')
-
-def listar_produto():
-    messagebox.showinfo('Listar produto')
-
-
-# Criação da janela principal
-root = tk.Tk()
-root.title("Cadastro e Localização de Produtos")
-
-# Labels e entradas de dados
-tk.Label(root, text="Nome do Produto:").pack(pady=5)
-entry_nome = tk.Entry(root)
-entry_nome.pack(pady=5)
-
-tk.Label(root, text="Preço:").pack(pady=5)
-entry_preco = tk.Entry(root)
-entry_preco.pack(pady=5)
-
-tk.Label(root, text="Quantidade:").pack(pady=5)
-entry_quantidade = tk.Entry(root)
-entry_quantidade.pack(pady=5)
-
-# Botões
-btn_cadastrar = tk.Button(root, text="Cadastrar Produto", command=cadastrar_produto)
-btn_cadastrar.pack(pady=10)
-
-btn_localizar = tk.Button(root, text="Localizar Produto", command=localizar_produto)
-btn_localizar.pack(pady=10)
-
-btn_listar = tk.Button(root, text="Listar Produto", command=listar_produto)
-btn_listar.pack(pady=10)
-
-btn_sair = tk.Button(root, text="Sair", command=root.quit)
-btn_sair.pack(pady=10)
-
-# Inicia o loop principal da interface gráfica
-root.mainloop()
->>>>>>> 3207bc9377864f8daa63b28bb977930aa4d354e5
